30014ef9762fe5101d17717172a1fa55b8dce2cd
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-09-26 12:20:55 -0400

    Create Ex1_HelloWorld.py

---
+++ Ex1_HelloWorld.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 09:49:40 2023
+
+@author: Jinjia
+"""
+
+"""
+Write a program, when run, greets the user by printing "Hello, world!" on the screen. 
+Then it prints a message on the screen asking the user to enter their name. 
+The program greets the user by name by printing the "Hello," followed by the user's name.
+Prerequisite concepts: print(), strings, string concatenation
+"""
+
+print("Hello, world!")
+name = input("What is your name? ")
+print(f"Hello, {name}")
