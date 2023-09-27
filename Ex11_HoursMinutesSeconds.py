63cb9dc079e54d12de9f84c942dc3a857f38c694
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-09-27 00:05:27 -0400

    Create Ex11_HoursMinutesSeconds.py

---
+++ Ex11_HoursMinutesSeconds.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 23:46:52 2023
+
+@author: Jinjia
+"""
+
+"""
+Write a getHoursMinutesSeconds() function that has a totalSeconds
+parameter. The argument for this parameter will be the number of 
+seconds to be translated into the number of hours, minutes, and
+seconds. If the amount for the hours, minutes, or seconds is zero,
+don't show it.
+
+Additional challenge: break up 24 hour periods into days with a 
+"d" suffix.
+
+Prerequisite concepts: join(), append(), lists, string concatenation, 
+while loops
+"""
+
+
+def getHoursMinutesSeconds(totalSeconds):
+    # transfer totalSeconds into hours, minutes, and seconds.
+    
