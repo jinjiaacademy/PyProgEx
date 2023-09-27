f1c3f5a2dad4f3afa2a78d0a87b2e83e969480d9
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-09-27 00:05:24 -0400

    Update Ex10_FindAndReplace.py

--- Ex10_FindAndReplace.py
+++ Ex10_FindAndReplace.py
@@ -5,3 +5,24 @@ Created on Tue Sep 26 14:22:23 2023
 @author: Jinjia
 """
 
+"""
+Write a findAndReplace() function that has three parameters: text
+is the string with text to be replaced, oldText is the text to be
+replaced, and newText is the replacement text.
+This function must be case-sensitive.
+
+Prerequisite concepts: slices, indexes, len(), augmented assignment
+operator
+"""
+
+
+def findAndReplace(text, oldText, newText):
+    # replace the oldText in text with newText
+    
+
+
+# assert findAndReplace('The fox', 'fox', 'dog') == 'The dog'
+# assert findAndReplace('fox', 'fox', 'dog') == 'dog'
+# assert findAndReplace('Firefox', 'fox', 'dog') == 'Firedog'
+# assert findAndReplace('foxfox', 'fox', 'dog') == 'dogdog'
+# assert findAndReplace('The Fox and fox.', 'fox', 'dog') == 'The Fox and dog.'
