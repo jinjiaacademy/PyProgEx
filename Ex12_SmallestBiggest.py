2a8e8474d8cd1b48e35e5d76427a776a4de617f2
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-09-27 00:05:29 -0400

    Create Ex12_SmallestBiggest.py

---
+++ Ex12_SmallestBiggest.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 27 00:00:41 2023
+
+@author: Jinjia
+"""
+
+"""
+Write a getSmallest() function that has a numbers parameter.
+The numbers parameter will be a list of integer and floating-point
+number values. The function returns the smallest value in the list.
+If the list is empty, the function should return None.
+When you are done with this exercise, write a getBiggest() function
+which returns the biggest number instead of the smallest number.
+
+Prerequisite concepts: len(), for loops, lists, None value
+"""
+
+
