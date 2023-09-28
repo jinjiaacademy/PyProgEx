71301c3aa604bc82e53a159e2ddf08bd83698e97
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-09-28 11:20:27 -0400

    Update Ex12_SmallestBiggest.py

--- Ex12_SmallestBiggest.py
+++ Ex12_SmallestBiggest.py
@@ -17,3 +17,33 @@ Prerequisite concepts: len(), for loops, lists, None value
 """
 
 
+def getSmallest(numbers):
+    # return the smallest number in numbers list, return None
+    # if the numbers list is empty
+    if len(numbers) == 0:
+        return None
+    
+    smallestNum = numbers[0]
+    for number in numbers:
+        if number < smallestNum:
+            smallestNum = number
+    return smallestNum
+
+def getBiggest(numbers):
+    # return the biggest number in numbers list, return None
+    # if the numbers list is empty
+    if len(numbers) == 0:
+        return None
+    
+    biggestNum = numbers[0]
+    for number in numbers:
+        if number > biggestNum:
+            biggestNum = number
+    return biggestNum
+
+
+assert getSmallest([1, 2, 3]) == 1
+assert getSmallest([3, 2, 1]) == 1
+assert getSmallest([28, 25, 42, 2, 28]) == 2
+assert getSmallest([1]) == 1
+assert getSmallest([]) == None
