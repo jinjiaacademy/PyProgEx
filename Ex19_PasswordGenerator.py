ff31cb7a4ba356f18cb0b3933cba114dd569b2ea
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-09-28 12:11:14 -0400

    Create Ex19_PasswordGenerator.py

---
+++ Ex19_PasswordGenerator.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 28 12:04:26 2023
+
+@author: Jinjia
+"""
+
+"""
+Write a generatePassword() function that has a length parameter. The length
+parameter is an integer of how many characters the generated password should
+have. For security reasons, if length is less than 12, the function forcibly
+sets it to 12 characters anyway. 
+The password string returned by the function must have at least one lowercase,
+one uppercase, one number, and one special character ~!@#$%^&*()_+.
+
+Prerequisite concepts: import statements, random module, strings, string
+concatenation, len(), append(), randint(), shuffle(), join()
+"""
