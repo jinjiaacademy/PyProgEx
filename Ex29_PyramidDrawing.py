840cb51147e02cc9cff4c5024c301d6483c5915e
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-09-29 09:27:44 -0400

    Update Ex29_PyramidDrawing.py

--- Ex29_PyramidDrawing.py
+++ Ex29_PyramidDrawing.py
@@ -5,3 +5,18 @@ Created on Thu Sep 28 23:07:33 2023
 @author: Jinjia
 """
 
+"""
+Write a drawPyramid() function with a height parameter. The top of the 
+pyramid has one centered hashtag character, and the subsequent rows have 
+two more hashtags thatn the previous row.
+
+Prerequisite concepts: strings, string concatenation, string replication,
+for loops, range()
+"""
+
+def drawPyramid(height):
+    # draw a pyramid with hashtag #
+    for i in range(height):
+        print(" " * (height - i - 1) + "#" * (2 * i + 1))
+        
+drawPyramid(10)
