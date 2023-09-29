a110dea5d69cb3e4694540daf29be92a03b5c0bf
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-09-29 09:27:49 -0400

    Create Ex31_ConvertIntegersToStrings.py

---
+++ Ex31_ConvertIntegersToStrings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 29 09:23:14 2023
+
+@author: Jinjia
+"""
+
+"""
+Write a convertIntToStr() function with an integerNum parameter. This function
+operates similarly to the str() function in that it returns a string form of
+the parameter. 
+
+Prerequisite concepts: dictionaries, while loops, string concatenation,
+integer division
+"""
+
+
